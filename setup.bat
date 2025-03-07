2b255e25ba832dfbf1cbe98ad15c2c6ccb65a594
AdrianD234 <121910059+AdrianD234@users.noreply.github.com>
2025-03-07 17:23:04 +1300

    Delete setup.bat

--- setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing required packages...
-pip install -r requirements.txt
-
-echo Setting up the environment...
-echo Done! You can now run the dashboard by executing 'python app.py'
